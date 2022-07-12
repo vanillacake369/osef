d3718bddc4b1708a3f07c684e9952d326a5baff0
groot <99851831+hwang-won@users.noreply.github.com>
2022-07-12 13:37:38 +0900

    JS : arrow btn

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 // introduce navbar up - 스크롤하면 navbar 나옴
 const navbar = document.querySelector('.intro_navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', ()=> {
+document.addEventListener('scroll', () => {
     // console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
+    // console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > 410) {
         navbar.classList.add('intro_navbar_up');
     } else {
@@ -26,3 +26,25 @@ navbarMenu.addEventListener('click', (event) => {
     scrollTo.scrollIntoView({behavior: "smooth"});    
     
 })
+
+// arrow scrollIntoView button - navbar 나오면 버튼 출현
+const arrow = document.querySelector('.arrow');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > 410) {
+        arrow.classList.add('visible');
+    } else {
+        arrow.classList.remove('visible');
+    }
+})
+
+// arrow scrollIntoView button - 당근 클릭하면 상단으로 이동
+arrow.addEventListener('click', () => {
+    scrollIntoView('#navbar');
+})
+
+// function scrollIntoView - 클릭시 해당 data(id)로 이돈
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'})
+}
+
