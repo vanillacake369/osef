72550eaa72b54c7e70bb8ec5d4935535c555aa4c
groot <99851831+hwang-won@users.noreply.github.com>
2022-07-15 17:28:02 +0900

    html : lend 뒤로가기 기능 추가

--- lend.js
+++ lend.js
@@ -12,6 +12,7 @@ const lendFormA4 = document.querySelector('#lend_form_A4');
 const lendFormA5 = document.querySelector('#lend_form_A5');
 const lendFormA6 = document.querySelector('#lend_form_A6');
 const lendFormA7 = document.querySelector('#lend_form_A7');
+const lendFormA8 = document.querySelector('#lend_form_A8');
 
 const lendFormQ1 = document.querySelector('#lend_Q1');
 const lendFormQ2 = document.querySelector('#lend_Q2');
@@ -20,6 +21,7 @@ const lendFormQ4 = document.querySelector('#lend_Q4');
 const lendFormQ5 = document.querySelector('#lend_Q5');
 const lendFormQ6 = document.querySelector('#lend_Q6');
 const lendFormQ7 = document.querySelector('#lend_Q7');
+const lendFormQ8 = document.querySelector('#lend_Q8');
 
 const nodeCategory = document.getElementsByName('category');
 
@@ -27,8 +29,9 @@ const lendInputA2 = document.querySelector('#lend_form_A2 input');
 const lendInputA3 = document.querySelector('#lend_form_A3 input');
 const lendInputA41 = document.querySelector('#lend_form_A4_start input');
 const lendInputA42 = document.querySelector('#lend_form_A4_end input');
-
-
+const lendInputA5 = document.querySelector('#lend_form_A5 #file');
+const lendInputA6 = document.querySelector('#lend_form_A6 textarea');
+const lendInputA7 = document.querySelector('#lend_form_A7 input');
 
 const lendBtn = document.querySelector('#lend_next_btn');
 
@@ -82,6 +85,17 @@ function onLendSumbitA2(event) {
 }
 lendFormA2.addEventListener('submit', onLendSumbitA2);
 
+// 뒤로가기 버튼
+const lendFormA2Btn = document.querySelector('#A2_btn');
+lendFormA2Btn.addEventListener('click', (event) => {
+    lendFormQ2.classList.add(HIDDEN_CLASSNAME);
+    lendFormA2.classList.add(HIDDEN_CLASSNAME);
+
+    lendFormQ1.classList.remove(HIDDEN_CLASSNAME);
+    lendFormA1.classList.remove(HIDDEN_CLASSNAME);
+
+})
+
 // lend_form_A3
 function onLendSumbitA3(event) {
     event.preventDefault(); 
@@ -96,6 +110,17 @@ function onLendSumbitA3(event) {
     lendFormA4.classList.remove(HIDDEN_CLASSNAME);
 }
 lendFormA3.addEventListener('submit', onLendSumbitA3);
+
+// 뒤로가기 버튼
+const lendFormA3Btn = document.querySelector('#A3_btn');
+lendFormA3Btn.addEventListener('click', (event) => {
+    lendFormQ3.classList.add(HIDDEN_CLASSNAME);
+    lendFormA3.classList.add(HIDDEN_CLASSNAME);
+
+    lendFormQ2.classList.remove(HIDDEN_CLASSNAME);
+    lendFormA2.classList.remove(HIDDEN_CLASSNAME);
+
+})
 
 // lend_form_A4
 function onLendSumbitA4(event) {
@@ -114,3 +139,107 @@ function onLendSumbitA4(event) {
 }
 lendFormA4.addEventListener('submit', onLendSumbitA4);
 
+// 뒤로가기 버튼
+const lendFormA4Btn = document.querySelector('#A4_btn');
+lendFormA4Btn.addEventListener('click', (event) => {
+    lendFormQ4.classList.add(HIDDEN_CLASSNAME);
+    lendFormA4.classList.add(HIDDEN_CLASSNAME);
+
+    lendFormQ3.classList.remove(HIDDEN_CLASSNAME);
+    lendFormA3.classList.remove(HIDDEN_CLASSNAME);
+
+})
+
+// lend_form_A5
+function onLendSumbitA5(event) {
+    event.preventDefault(); 
+
+    const flie = lendInputA5.value;
+    localStorage.setItem('flie', flie);
+
+    lendFormQ5.classList.add(HIDDEN_CLASSNAME);
+    lendFormA5.classList.add(HIDDEN_CLASSNAME);
+
+    lendFormQ6.classList.remove(HIDDEN_CLASSNAME);
+    lendFormA6.classList.remove(HIDDEN_CLASSNAME);
+}
+lendFormA5.addEventListener('submit', onLendSumbitA5);
+
+// 뒤로가기 버튼
+const lendFormA5Btn = document.querySelector('#A5_btn');
+lendFormA5Btn.addEventListener('click', (event) => {
+    lendFormQ5.classList.add(HIDDEN_CLASSNAME);
+    lendFormA5.classList.add(HIDDEN_CLASSNAME);
+
+    lendFormQ4.classList.remove(HIDDEN_CLASSNAME);
+    lendFormA4.classList.remove(HIDDEN_CLASSNAME);
+
+})
+
+// 이미지 미리보기
+function readURL (input) {
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('preview').src = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        document.getElementById('preview').src = "";
+    }
+
+    const preview = document.querySelector('#preview');
+    preview.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// lend_form_A6
+function onLendSumbitA6(event) {
+    event.preventDefault(); 
+
+    const textarea = lendInputA6.value;
+    localStorage.setItem('textarea', textarea);
+
+    lendFormQ6.classList.add(HIDDEN_CLASSNAME);
+    lendFormA6.classList.add(HIDDEN_CLASSNAME);
+
+    lendFormQ7.classList.remove(HIDDEN_CLASSNAME);
+    lendFormA7.classList.remove(HIDDEN_CLASSNAME);
+}
+lendFormA6.addEventListener('submit', onLendSumbitA6);
+
+// 뒤로가기 버튼
+const lendFormA6Btn = document.querySelector('#A6_btn');
+lendFormA6Btn.addEventListener('click', (event) => {
+    lendFormQ6.classList.add(HIDDEN_CLASSNAME);
+    lendFormA6.classList.add(HIDDEN_CLASSNAME);
+
+    lendFormQ5.classList.remove(HIDDEN_CLASSNAME);
+    lendFormA5.classList.remove(HIDDEN_CLASSNAME);
+
+})
+
+// lend_form_A7
+function onLendSumbitA7(event) {
+    event.preventDefault(); 
+
+    const price = lendInputA7.value;
+    localStorage.setItem('price', price);
+
+    lendFormQ7.classList.add(HIDDEN_CLASSNAME);
+    lendFormA7.classList.add(HIDDEN_CLASSNAME);
+
+    lendFormQ8.classList.remove(HIDDEN_CLASSNAME);
+    lendFormA8.classList.remove(HIDDEN_CLASSNAME);
+}
+lendFormA7.addEventListener('submit', onLendSumbitA7);
+
+// 뒤로가기 버튼
+const lendFormA7Btn = document.querySelector('#A7_btn');
+lendFormA7Btn.addEventListener('click', (event) => {
+    lendFormQ7.classList.add(HIDDEN_CLASSNAME);
+    lendFormA7.classList.add(HIDDEN_CLASSNAME);
+
+    lendFormQ6.classList.remove(HIDDEN_CLASSNAME);
+    lendFormA6.classList.remove(HIDDEN_CLASSNAME);
+
+})
